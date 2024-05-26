8eee704aca4baf0fe9a104816b510e47a8bb29e2
Lena-15 <lenrubtsova68@gmail.com>
2024-05-27 01:26:17 +0300

    Home Work 1 change agane

---
+++ src/HW1/Student.java
@@ -0,0 +1,23 @@
+package HW1;
+public class Student {
+
+    public String name;
+    public int mark;
+
+    public String getName() {
+            return name;
+    }
+
+    public void setName(String studentName) {
+            name = studentName;
+    }
+
+    public int getMark() {
+            return mark;
+    }
+
+    public void setMark(int studentMark) {
+            mark = studentMark;
+    }
+}
+
